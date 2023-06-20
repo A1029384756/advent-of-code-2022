78db74f846e25e3239f4e369c6db2b2b3c4595f9
Hayden Gray <hayden.gray104@gmail.com>
2023-06-19 22:22:47 -0400

    Day 6

---
+++ src/day_6.rs
@@ -0,0 +1,55 @@
+use std::{fs::read_to_string, collections::HashSet};
+
+fn find_marker(input: &str, n: usize) -> usize {
+    input.as_bytes().windows(n).position(|window| window.iter().collect::<HashSet<_>>().len() == n).unwrap() + n
+}
+
+fn part_1(input: &str) -> usize {
+    find_marker(input, 4)
+}
+
+fn part_2(input: &str) -> usize {
+    find_marker(input, 14)
+}
+
+fn main() {
+    let input = read_to_string("./test_files/day_6.txt").unwrap();
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+#[test]
+fn test_part_1() {
+    let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    assert_eq!(part_1(input), 7);
+
+    let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    assert_eq!(part_1(input), 5);
+
+    let input = "nppdvjthqldpwncqszvftbrmjlhg";
+    assert_eq!(part_1(input), 6);
+
+    let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    assert_eq!(part_1(input), 10);
+
+    let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+    assert_eq!(part_1(input), 11);
+}
+
+#[test]
+fn test_part_2() {
+    let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    assert_eq!(part_2(input), 19);
+
+    let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    assert_eq!(part_2(input), 23);
+
+    let input = "nppdvjthqldpwncqszvftbrmjlhg";
+    assert_eq!(part_2(input), 23);
+
+    let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    assert_eq!(part_2(input), 29);
+
+    let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+    assert_eq!(part_2(input), 26);
+}
