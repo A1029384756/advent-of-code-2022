ad6e2f7dd9c5241780d0a2f133659907230d5dd7
A1029384756 <hayden.gray104@gmail.com>
2023-06-28 21:49:06 -0400

    Day 14 visualization - using software rendering

--- src/day_14.rs
+++ src/day_14.rs
@@ -2,8 +2,11 @@
 #![feature(generators)]
 #![feature(iter_from_generator)]
 
-use std::fmt;
+use std::{fmt, time::Duration};
 
+use egui::{ColorImage, Slider, TextureOptions};
+
+use image::ImageBuffer;
 use nom::{
     bytes::complete::tag, character::complete as cc, multi::separated_list1, sequence::tuple,
     Finish, IResult,
@@ -112,6 +115,10 @@ struct Grid {
     data: Vec<Unit>,
     grains: Vec<Coord>,
     settled: i32,
+    speed: u32,
+    paused: bool,
+    step: bool,
+    img: Option<egui::TextureHandle>,
 }
 
 impl Grid {
@@ -135,16 +142,22 @@ impl Grid {
             max_x = max_x.max(point.x);
             max_y = max_y.max(point.y);
         }
-        
+
         let floor_y = max_y + 2;
-        min_x = 200;
-        max_x = 800;
+        min_x = 300;
+        max_x = 700;
         max_y = floor_y;
         lines.push(Line {
             points: vec![
-                Coord { x: min_x, y:floor_y },
-                Coord { x: max_x, y:floor_y },
-            ]
+                Coord {
+                    x: min_x,
+                    y: floor_y,
+                },
+                Coord {
+                    x: max_x,
+                    y: floor_y,
+                },
+            ],
         });
 
         let origin = Coord { x: min_x, y: min_y };
@@ -158,6 +171,10 @@ impl Grid {
             data: vec![Unit::Air; width * height],
             grains: vec![],
             settled: 0,
+            speed: 1,
+            paused: true,
+            step: false,
+            img: None,
         };
 
         for point in lines.iter().flat_map(|p| p.path_points()) {
@@ -188,6 +205,10 @@ impl Grid {
     }
 
     fn step(&mut self) {
+        if matches!(self.get_unit(Coord { x: 500, y: 0 }).unwrap(), Unit::Sand) {
+            return;
+        }
+
         let mut grains = std::mem::take(&mut self.grains);
         let _ = grains
             .extract_if(|grain| {
@@ -240,20 +261,96 @@ impl fmt::Debug for Grid {
     }
 }
 
-fn solve() -> i32 {
-    let mut g = Grid::new();
+impl eframe::App for Grid {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.button("Reset").clicked() {
+                    *self = Self::new();
+                }
 
-    loop {
-        g.step();
+                if ui.button("Step").clicked() {
+                    self.step();
+                }
 
-        if matches!(g.get_unit(Coord { x: 500, y: 0 }).unwrap(), Unit::Sand) {
-            break;
+                let paused = self.paused;
+                ui.toggle_value(&mut self.paused, if paused { "▶" } else { "⏸" });
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Speed: ");
+                ui.add(Slider::new(&mut self.speed, 1..=20).prefix("x"));
+            });
+        });
+
+        if self.step {
+            self.step();
+            self.step = false;
+        } else if !self.paused {
+            (0..self.speed).for_each(|_| {
+                self.step();
+            });
+            ctx.request_repaint_after(Duration::from_millis(25));
         }
-    }
 
-    g.settled
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let mut buff = ImageBuffer::new(self.width as _, self.height as _);
+
+            for pixel in buff.pixels_mut() {
+                *pixel = image::Rgba([255, 20, 20, 255]);
+            }
+
+            let style = &ctx.style().visuals;
+            let air = style.window_fill();
+
+            let air_color: [u8; 4] = [air.r(), air.g(), air.b(), air.a()];
+            let rock_color: [u8; 4] = [160, 160, 160, 255];
+            let sand_color: [u8; 4] = [130, 127, 88, 255];
+            let curr_color: [u8; 4] = [245, 206, 49, 255];
+
+            for (x, y, pixel) in buff.enumerate_pixels_mut() {
+                let coord = Coord {
+                    x: x as _,
+                    y: y as _,
+                } + self.origin;
+
+                let unit = self.get_unit(coord).unwrap();
+                let color = match unit {
+                    Unit::Air => &air_color,
+                    Unit::Rock => &rock_color,
+                    Unit::Sand => &sand_color,
+                };
+
+                *pixel = image::Rgba(*color);
+            }
+
+            for grain in self.grains.iter().copied() {
+                let Coord { x, y } = grain - self.origin;
+                buff.put_pixel(x as _, y as _, image::Rgba(curr_color));
+            }
+
+            let img =
+                ColorImage::from_rgba_unmultiplied([buff.width() as _, buff.height() as _], &buff);
+
+            self.img = Some(ui.ctx().load_texture("", img, TextureOptions::NEAREST));
+
+            if let Some(img) = self.img.as_ref() {
+                ui.image(img, ui.available_size());
+            }
+        });
+    }
 }
 
 fn main() {
-    println!("{}", solve());
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1280.0, 720.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Advent of Code 2022 - Day 9",
+        options,
+        Box::new(|_cc| Box::new(Grid::new())),
+    )
+    .expect("eframe failed to start");
 }
